803d5fe03fe7cac86883c80a59d8c44492a341d9
Dan Park <dpark.sports@gmail.com>
2019-07-18 20:20:34 -0700

    Update matrix.cpp

--- matrix.cpp
+++ matrix.cpp
@@ -1,38 +1,6 @@
-/*
-Let's start by writing our own Matrix class!
-
-It should:
-* Work for any size 2D matrix of type double
-* Access and set any particular element, safely
-* print in a human-readable way
-* Support multiplication with a scalar
-* Support multiplication with another Matrix
-* Make sure to test it!
-* NxN 
-
-m1:
-7 4 7
-6 2 3
-1 2 2
-
-m2:
-5 4 7
-2 1 8
-5 4 8
-
-m1 * m2:
-78 60 137
-49 38 82
-19 14 39
-
-m3:
-7 4 7 9
-6 2 3 6
-1 1 1 1
-4 3 5 8
-*/
-
 #include <iostream>
+#include <vector>
+
 using namespace std;
 
 class Matrix {
@@ -40,7 +8,6 @@ class Matrix {
   private mutex mutex;
   
   Matrix() { }
-  // implement me!
   
   void init(const int &m, const int &n, const int &intValue) {
    vector<vector<double>> matrixA(m, vector<double>(n, initValue));
