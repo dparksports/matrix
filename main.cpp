4c72d10d5b95f887a9f5282408373de12ab90ea2
Dan Park <dpark.sports@gmail.com>
2019-07-21 23:53:53 -0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -418,8 +418,72 @@ int scanStudentStruct() {
     cout << endl;
 }
 
+
+#include <iostream>
+#include <string>
+#include <sstream>
+#include <exception>
+using namespace std;
+
+class BadLengthException: public exception {
+private:
+    int length;
+    string exceptionString;
+
+public:
+    BadLengthException(int n) {
+        length = n;
+        exceptionString = to_string(n);
+    }
+
+    virtual const char* what() const throw()
+    {
+//        string temp;
+//        temp.append(to_string(length));
+//        exceptionString.append(temp);
+        return exceptionString.c_str();
+    }
+};
+
+bool checkUsername(string username) {
+    bool isValid = true;
+    int n = username.length();
+    if(n < 5) {
+        throw BadLengthException(n);
+    }
+    for(int i = 0; i < n-1; i++) {
+        if(username[i] == 'w' && username[i+1] == 'w') {
+            isValid = false;
+        }
+    }
+    return isValid;
+}
+
+int throwException() {
+    int T; cin >> T;
+    while(T--) {
+        string username;
+        cin >> username;
+        try {
+            bool isValid = checkUsername(username);
+            if(isValid) {
+                cout << "Valid" << '\n';
+            } else {
+                cout << "Invalid" << '\n';
+            }
+        } catch (BadLengthException e) {
+            cout << "Too short: " << e.what() << '\n';
+
+            cout << endl;
+        }
+    }
+    cout << endl;
+    return 0;
+}
+
 int main(int argc, const char *argv[]) {
-    scanStudentStruct();
+    throwException();
+//    scanStudentStruct();
 //    scanName();
 //    scanStudents();
 
